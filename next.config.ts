af8d6da851cad9ae0d0912d44a2fb6b9520a96c4
Prince Saini <princesaini7963@gmail.com>
2025-04-30 16:34:03 +0530

    Fix: bugs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['https://resume-scanning-and-filteration-production.up.railway.app/'], // Replace with your actual backend domain
   },
