45e7657b4abb813083ccb86871cf31e055ba3218
Prince Saini <princesaini7963@gmail.com>
2025-04-30 17:37:12 +0530

    Feat: Flask App url added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['https://resume-scanning-and-filteration-production.up.railway.app/'], // Replace with your actual backend domain
+    domains: ['https://web-production-3e7e.up.railway.app/'], // Replace with your actual backend domain
   },
 };
 
